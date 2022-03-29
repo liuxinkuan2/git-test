c4bc63fb437e3f60d68d2169f993475d6295542e
lxk <lxk@xja.com>
2022-03-29 21:27:02 +0800

    first commit

---
+++ src/main/java/com/xja/entity/Student.java
@@ -0,0 +1,12 @@
+package com.xja.entity;
+
+/**
+ * @author 柳新宽
+ * @create 2022-03-29  21:24
+ * @describe
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
